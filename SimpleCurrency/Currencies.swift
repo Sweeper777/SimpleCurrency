53e6a2435ab9a896e356bfcdae223c96e18d369b
Sweeper777 <sumulang@gmail.com>
2017-02-22 06:39:14 +0000

    added Currencies enum

---
+++ SimpleCurrency/Currencies.swift
@@ -0,0 +1,7 @@
+enum Currencies : CustomStringConvertible {
+    case AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, GBP, HKD, HRK, HUF, IDR, ILS, INR, JPY, MRW, MXN, MYR, NOK, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR, EUR
+    
+    var description: String {
+        return "\(self)"
+    }
+}
