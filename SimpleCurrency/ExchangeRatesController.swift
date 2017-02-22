6ca3579ed2ad61fdc6c66aa7104c42e201c39141
Sweeper777 <sumulang@gmail.com>
2017-02-22 06:39:37 +0000

    added method stubs for ExchangeRatesController

---
+++ SimpleCurrency/ExchangeRatesController.swift
@@ -0,0 +1,26 @@
+import UIKit
+import ESPullToRefresh
+import Alamofire
+
+class ExchangeRatesController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return nil
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+}
+
