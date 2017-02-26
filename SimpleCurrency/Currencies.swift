eed58a8d8e1bb050370011265a7418c5e26c7da0
Sweeper777 <sumulang@gmail.com>
2017-02-26 08:14:41 +0000

    added property to get all values

--- SimpleCurrency/Currencies.swift
+++ SimpleCurrency/Currencies.swift
@@ -4,4 +4,8 @@ enum Currencies : CustomStringConvertible {
     var description: String {
         return "\(self)"
     }
+    
+    static var allValues: [Currencies] {
+        return [.AUD, .BGN, .BRL, .CAD, .CHF, .CNY, .CZK, .DKK, .GBP, .HKD, .HRK, .HUF, .IDR, .ILS, .INR, .JPY, .MRW, .MXN, .MYR, .NOK, .PHP, .PLN, .RON, .RUB, .SEK, .SGD, .THB, .TRY, .USD, .ZAR, .EUR]
+    }
 }
