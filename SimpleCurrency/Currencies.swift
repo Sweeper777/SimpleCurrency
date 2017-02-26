04a8a5eb5566e517417b097db6a82ffa9e6f079e
Sweeper777 <sumulang@gmail.com>
2017-02-26 09:00:38 +0000

    completely changed how Currency enum works

--- SimpleCurrency/Currencies.swift
+++ SimpleCurrency/Currencies.swift
@@ -1,11 +1,47 @@
-enum Currencies : CustomStringConvertible {
-    case AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, GBP, HKD, HRK, HUF, IDR, ILS, INR, JPY, MRW, MXN, MYR, NOK, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR, EUR
+enum Currencies {
+    case AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, IDR, ILS, INR, JPY, KRW, MXN, MYR, NOK, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR
     
-    var description: String {
+    var currencyCode: String {
         return "\(self)"
     }
     
     static var allValues: [Currencies] {
-        return [.AUD, .BGN, .BRL, .CAD, .CHF, .CNY, .CZK, .DKK, .GBP, .HKD, .HRK, .HUF, .IDR, .ILS, .INR, .JPY, .MRW, .MXN, .MYR, .NOK, .PHP, .PLN, .RON, .RUB, .SEK, .SGD, .THB, .TRY, .USD, .ZAR, .EUR]
+        return [.AUD, .BGN, .BRL, .CAD, .CHF, .CNY, .CZK, .DKK, .EUR, .GBP, .HKD, .HRK, .HUF, .IDR, .ILS, .INR, .JPY, .KRW, .MXN, .MYR, .NOK, .PHP, .PLN, .RON, .RUB, .SEK, .SGD, .THB, .TRY, .USD, .ZAR]
+    }
+    
+    var fullNameDict: [Currencies:String] {
+        return [
+            .AUD: "Australian Dollar",
+            .BGN: "Bulgarian Lev",
+            .BRL: "Brazilian Real",
+            .CAD: "Canadian Dollar",
+            .CHF: "Swiss Franc",
+            .CNY: "Yuan Renminbi",
+            .CZK: "Czech Koruna",
+            .DKK: "Danish Krone",
+            .EUR: "Euro",
+            .GBP: "Pound Sterling",
+            .HKD: "Hong Kong Dollar",
+            .HRK: "Croatian Kuna",
+            .HUF: "Hungarian Forint",
+            .IDR: "Indonesian Rupiah",
+            .ILS: "Israeli New Shekel",
+            .INR: "Indian Rupee",
+            .JPY: "Japanese Yen",
+            .KRW: "Korean Won",
+            .MXN: "Mexican Nuevo Peso",
+            .MYR: "Malaysian Ringgit",
+            .NOK: "Norwegian Krone",
+            .PHP: "Philippine Peso",
+            .PLN: "Polish Zloty",
+            .RON: "Romanian New Leu",
+            .RUB: "Russian Ruble",
+            .SEK: "Swedish Krona",
+            .SGD: "Singapore Dollar",
+            .THB: "Thai Baht",
+            .TRY: "Turkish Lira",
+            .USD: "US Dollar",
+            .ZAR: "South African Rand",
+        ]
     }
 }
