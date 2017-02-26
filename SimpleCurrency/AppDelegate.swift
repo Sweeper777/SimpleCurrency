48496c85012719b19c78a303a9a8cb78302df3d5
Sweeper777 <sumulang@gmail.com>
2017-02-26 08:26:44 +0000

    set baseCurrency and baseAmount on first start of app

--- SimpleCurrency/AppDelegate.swift
+++ SimpleCurrency/AppDelegate.swift
@@ -8,7 +8,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if lastUsedBuild == 0 {
+            UserDefaults.standard.set("USD", forKey: "baseCurrency")
+            UserDefaults.standard.set(1.0, forKey: "baseAmount")
+        }
+        
         lastUsedBuild = Int(Bundle.main.appBuild) ?? 0
         return true
     }
