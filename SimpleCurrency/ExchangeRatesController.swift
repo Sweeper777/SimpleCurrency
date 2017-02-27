6e698930e1c4a0bda67d5cf1535d4cb5ff94d64a
Sweeper777 <sumulang@gmail.com>
2017-02-27 07:39:35 +0000

    added some stubs for handling requests and stuff

--- SimpleCurrency/ExchangeRatesController.swift
+++ SimpleCurrency/ExchangeRatesController.swift
@@ -1,12 +1,34 @@
 import UIKit
 import ESPullToRefresh
 import Alamofire
+import SwiftyJSON
+import SCLAlertView
 
 class ExchangeRatesController: UITableViewController {
+    var baseCurrency: String!
+    var baseAmount: Double!
+    var currencies: [String]!
     
+    var json: JSON!
     
     override func viewDidLoad() {
-        
+        currencies = UserDefaults.standard.array(forKey: "currencies")!.map { $0 as! String }
+        baseCurrency = UserDefaults.standard.string(forKey: "baseCurrency")
+        baseAmount = UserDefaults.standard.double(forKey: "baseAmount")
+        Alamofire.request("https://api.fixer.io/latest?base=\(baseCurrency)&symbols=\(currencies.joined(separator: ","))&amount=\(baseAmount)").responseData {
+            [weak self]
+            response in
+            if let _ = response.error {
+                _ = SCLAlertView().showError(NSLocalizedString("Error", comment: ""), subTitle: NSLocalizedString("Unable to get exchange rates.", comment: ""))
+                return
+            }
+            
+            let json = JSON(data: response.value!)
+            if let _ = json["error"].string {
+                _ = SCLAlertView().showError(NSLocalizedString("Error", comment: ""), subTitle: NSLocalizedString("Unable to get exchange rates.", comment: ""))
+            }
+            self?.json = json
+        }
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
