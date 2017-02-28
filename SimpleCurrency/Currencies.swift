d379eb0cb305c994a43485800a70168233c3d27d
Sweeper777 <sumulang@gmail.com>
2017-02-28 06:53:49 +0000

    fixed some things in Currencies

--- SimpleCurrency/Currencies.swift
+++ SimpleCurrency/Currencies.swift
@@ -1,5 +1,5 @@
-enum Currencies {
-    case AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, IDR, ILS, INR, JPY, KRW, MXN, MYR, NOK, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR
+enum Currencies: String {
+    case AUD = "AUD", BGN = "BGN", BRL = "BRL", CAD = "CAD", CHF = "CHF", CNY = "CNY", CZK = "CZK", DKK = "DKK", EUR = "EUR", GBP = "GBP", HKD = "HKD", HRK = "HRK", HUF = "HUF", IDR = "IDR", ILS = "ILS", INR = "INR", JPY = "JPY", KRW = "KRW", MXN = "MXN", MYR = "MYR", NOK = "NOK", PHP = "PHP", PLN = "PLN", RON = "RON", RUB = "RUB", SEK = "SEK", SGD = "SGD", THB = "THB", TRY = "TRY", USD = "USD", ZAR = "ZAR"
     
     var currencyCode: String {
         return "\(self)"
@@ -9,7 +9,7 @@ enum Currencies {
         return [.AUD, .BGN, .BRL, .CAD, .CHF, .CNY, .CZK, .DKK, .EUR, .GBP, .HKD, .HRK, .HUF, .IDR, .ILS, .INR, .JPY, .KRW, .MXN, .MYR, .NOK, .PHP, .PLN, .RON, .RUB, .SEK, .SGD, .THB, .TRY, .USD, .ZAR]
     }
     
-    var fullNameDict: [Currencies:String] {
+    static var fullNameDict: [Currencies:String] {
         return [
             .AUD: "Australian Dollar",
             .BGN: "Bulgarian Lev",
