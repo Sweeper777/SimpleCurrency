0fa240029bc760407a1519a2a19f44bd917fad07
Sweeper777 <sumulang@gmail.com>
2017-03-02 06:58:00 +0000

    added currencies rows

--- SimpleCurrency/SettingsController.swift
+++ SimpleCurrency/SettingsController.swift
@@ -21,5 +21,18 @@ class SettingsController: FormViewController {
             row.value = UserDefaults.standard.integer(forKey: "baseAmount")
         }
         
+        let section = Section(NSLocalizedString("Currencies", comment: ""))
+        form +++ section
+        
+        for currency in Currencies.allValues {
+            section <<< CheckRow(currency.currencyCode) {
+                $0.title = currency.currencyCode
+                $0.value = (UserDefaults.standard.array(forKey: "currencies") as! [String]).contains(currency.currencyCode)
+                $0.cellStyle = .subtitle
+                $0.baseCell.imageView!.image = UIImage(named: currency.currencyCode)
+            }.cellUpdate { cell, _ in
+                    cell.detailTextLabel!.text = Currencies.fullNameDict[currency]!
+            }
+        }
     }
 }
