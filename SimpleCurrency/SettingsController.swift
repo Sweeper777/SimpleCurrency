92c4a3dc0b2650a9e96a6764ffd24006bc74214b
Sweeper777 <sumulang@gmail.com>
2017-03-02 06:57:10 +0000

    added settings title

--- SimpleCurrency/SettingsController.swift
+++ SimpleCurrency/SettingsController.swift
@@ -1,8 +1,12 @@
 import Eureka
+import UIKit
 
 class SettingsController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = NSLocalizedString("Settings", comment: "")
+        
     }
 }
