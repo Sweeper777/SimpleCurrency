d64ae6c54e723f6495858baa7bc08185a89f114b
Sweeper777 <sumulang@gmail.com>
2017-03-04 08:15:05 +0000

    completed CurrencySelectorRow

--- SimpleCurrency/CurrencySelectorRow.swift
+++ SimpleCurrency/CurrencySelectorRow.swift
@@ -2,5 +2,19 @@ import UIKit
 import Eureka
 
 class CurrencySelectorRow: SelectorRow<PushSelectorCell<Currencies>, CurrencySelectorController> {
+    required init(tag: String?, _ initializer: ((CurrencySelectorRow) -> ())) {
+        super.init(tag: tag)
+        initializer(self)
+        presentationMode = PresentationMode.show(controllerProvider: ControllerProvider.storyBoard(storyboardId: "CurrencySelectorController", storyboardName: "Main", bundle: nil), onDismiss: {
+            _ in
+        })
+        displayValueFor = {
+            guard let currency = $0 else { return "" }
+            return currency.currencyCode
+        }
+    }
     
+    required convenience init(tag: String?) {
+        self.init(tag: tag)
+    }
 }
