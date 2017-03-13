24e992339e949c5b46c6c744b4b0fda026f9b03c
Sweeper777 <sumulang@gmail.com>
2017-03-13 07:40:54 +0000

    added fullName property

--- SimpleCurrency/Currencies.swift
+++ SimpleCurrency/Currencies.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 enum Currencies: String {
     case AUD = "AUD", BGN = "BGN", BRL = "BRL", CAD = "CAD", CHF = "CHF", CNY = "CNY", CZK = "CZK", DKK = "DKK", EUR = "EUR", GBP = "GBP", HKD = "HKD", HRK = "HRK", HUF = "HUF", IDR = "IDR", ILS = "ILS", INR = "INR", JPY = "JPY", KRW = "KRW", MXN = "MXN", MYR = "MYR", NOK = "NOK", PHP = "PHP", PLN = "PLN", RON = "RON", RUB = "RUB", SEK = "SEK", SGD = "SGD", THB = "THB", TRY = "TRY", USD = "USD", ZAR = "ZAR"
     
@@ -5,11 +7,15 @@ enum Currencies: String {
         return "\(self)"
     }
     
+    var fullName: String {
+        return NSLocalizedString(Currencies.fullNameDict[self]!, comment: "")
+    }
+    
     static var allValues: [Currencies] {
         return [.AUD, .BGN, .BRL, .CAD, .CHF, .CNY, .CZK, .DKK, .EUR, .GBP, .HKD, .HRK, .HUF, .IDR, .ILS, .INR, .JPY, .KRW, .MXN, .MYR, .NOK, .PHP, .PLN, .RON, .RUB, .SEK, .SGD, .THB, .TRY, .USD, .ZAR]
     }
     
-    static var fullNameDict: [Currencies:String] {
+    private static var fullNameDict: [Currencies:String] {
         return [
             .AUD: "Australian Dollar",
             .BGN: "Bulgarian Lev",
