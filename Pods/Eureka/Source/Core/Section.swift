3e97b262aa9b7aa968342a5c21d3ac3f53489fcf
Sweeper777 <sumulang@gmail.com>
2018-11-08 07:23:41 +0000

    Revert "fixed a compiler error"
    This reverts commit 328ce071023fd852097965635db57b0baf7b7461.

--- Pods/Eureka/Source/Core/Section.swift
+++ Pods/Eureka/Source/Core/Section.swift
@@ -204,8 +204,6 @@ open class Section {
 
 
 extension Section : MutableCollection, BidirectionalCollection {
-    public typealias SubSequence = ArraySlice<BaseRow>
-    public typealias Index = Int
     
     //MARK: MutableCollectionType
     
