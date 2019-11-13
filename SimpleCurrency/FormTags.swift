ee330245f16121d5ee61b026ca93b81f32a2a8c5
Mulang Su <sumulang@gmail.com>
2019-11-13 08:33:21 +0000

    added new form tags

--- SimpleCurrency/FormTags.swift
+++ SimpleCurrency/FormTags.swift
@@ -9,3 +9,6 @@ let tagNothing = "nothing"
 let tagCurrency1Convert = "currency1Convert"
 let tagCurrency2Convert = "currency2Convert"
 let tagHistoriicalRates = "historicalRates"
+
+let tagTodayExtensionSettings = "todayExtensionSettings"
+let tagTodayExtensionCurrency = "todayExtensionCurrency"
