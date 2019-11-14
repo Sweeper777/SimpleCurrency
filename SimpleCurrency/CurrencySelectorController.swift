6c0e1d8ad1d17236ba621b5041cda482e91f8b11
Mulang Su <sumulang@gmail.com>
2019-11-14 07:04:52 +0000

    made cellForRowAt check for allowNilValues

--- SimpleCurrency/CurrencySelectorController.swift
+++ SimpleCurrency/CurrencySelectorController.swift
@@ -23,10 +23,23 @@ class CurrencySelectorController: UITableViewController, TypedRowControllerType 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel!.text = "\(Currencies.allValues[indexPath.row].currencyCode) (\(Currencies.allValues[indexPath.row].symbol))"
-        cell.imageView!.image = UIImage(named: Currencies.allValues[indexPath.row].currencyCode)
-        cell.detailTextLabel!.text = Currencies.allValues[indexPath.row].fullName
-        return cell
+        if allowNilValue {
+            if indexPath.row == 0 {
+                cell.textLabel?.text = NSLocalizedString("None", comment: "")
+                cell.detailTextLabel?.text = ""
+                return cell
+            } else {
+                cell.textLabel!.text = "\(Currencies.allCases[indexPath.row - 1].currencyCode) (\(Currencies.allCases[indexPath.row - 1].symbol))"
+                cell.imageView!.image = UIImage(named: Currencies.allCases[indexPath.row - 1].currencyCode)
+                cell.detailTextLabel!.text = Currencies.allCases[indexPath.row - 1].fullName
+                return cell
+            }
+        } else {
+            cell.textLabel!.text = "\(Currencies.allCases[indexPath.row].currencyCode) (\(Currencies.allCases[indexPath.row].symbol))"
+            cell.imageView!.image = UIImage(named: Currencies.allCases[indexPath.row].currencyCode)
+            cell.detailTextLabel!.text = Currencies.allCases[indexPath.row].fullName
+            return cell
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
