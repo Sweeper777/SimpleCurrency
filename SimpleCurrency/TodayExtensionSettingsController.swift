aabc50660af1a9cdc540b2c692a135d8281a8085
Mulang Su <sumulang@gmail.com>
2019-11-17 20:46:05 +0000

    removed the form controller stuff from today extension settings VC

--- SimpleCurrency/TodayExtensionSettingsController.swift
+++ SimpleCurrency/TodayExtensionSettingsController.swift
@@ -1,29 +1,16 @@
 import UIKit
 import Eureka
 
-class TodayExtensionSettingsController : FormViewController {
+class TodayExtensionSettingsController : UITableViewController {
+    
+    var selectedCurrencyIndices: [Int] = []
+    var availableCurrencies: [Currencies] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let section = Section("Currencies")
-        for i in 0..<3 {
-            section <<< CurrencySelectorRow(tag: tagTodayExtensionCurrency + "\(i)") {
-                row in
-                row.title = String(format: NSLocalizedString("Currency %d", comment: ""), i + 1)
-                row.value = UserDefaults.shared.string(forKey: "todayExtensionCurrency\(i)").flatMap(Currencies.init)
-            }
-            .onChange({ (row) in
-                UserDefaults.shared.set(row.value?.currencyCode, forKey: "todayExtensionCurrency\(i)")
-            })
         }
-        form +++ section
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let vc = segue.destination as? CurrencySelectorController {
-            vc.allowNilValue = true
-            vc.row = (sender as! RowOf<Currencies>)
         }
     }
 }
