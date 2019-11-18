df7100ea2e6e572d3ac2d84ef0a53db97c3177e8
Mulang Su <sumulang@gmail.com>
2019-11-18 18:29:57 +0000

    implemented numberOfSections and numberOfRows

--- SimpleCurrency/TodayExtensionSettingsController.swift
+++ SimpleCurrency/TodayExtensionSettingsController.swift
@@ -12,6 +12,16 @@ class TodayExtensionSettingsController : UITableViewController {
         availableCurrencies = Currencies.allCases.filter { $0.currencyCode != baseCurrency }
         let selectedCurrencies = UserDefaults.shared.array(forKey: "todayExtensionCurrencies") as! [String]
         selectedCurrencyIndices = selectedCurrencies.compactMap(Currencies.init).compactMap(availableCurrencies.firstIndex)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return availableCurrencies.count
+    }
+    
         }
     }
     
