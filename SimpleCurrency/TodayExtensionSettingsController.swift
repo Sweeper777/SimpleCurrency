b9b9538855cfe641e09e6e236d7f24da3a7bd304
Mulang Su <sumulang@gmail.com>
2019-11-18 18:29:34 +0000

    initialised properties in today extension settings vC

--- SimpleCurrency/TodayExtensionSettingsController.swift
+++ SimpleCurrency/TodayExtensionSettingsController.swift
@@ -8,6 +8,10 @@ class TodayExtensionSettingsController : UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let baseCurrency = UserDefaults.shared.string(forKey: "baseCurrency")
+        availableCurrencies = Currencies.allCases.filter { $0.currencyCode != baseCurrency }
+        let selectedCurrencies = UserDefaults.shared.array(forKey: "todayExtensionCurrencies") as! [String]
+        selectedCurrencyIndices = selectedCurrencies.compactMap(Currencies.init).compactMap(availableCurrencies.firstIndex)
         }
     }
     
